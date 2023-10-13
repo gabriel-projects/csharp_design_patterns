732a4ec8abf243e3bbc1a678db0fde81d03a1b3b
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-10-13 00:04:13 -0300

    produtos de treinos

---
+++ Udemy_1/Creational/DesignPatterns/PatternsCriational/AbstractFactory/Exemplo_1/Products/Products/TrainingTuesdayFriday.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.PatternsCriational.AbstractFactory.Exemplo_1.Products.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.PatternsCriational.AbstractFactory.Exemplo_1.Products.Products
+{
+    public class TrainingTuesdayFriday : ITraining
+    {
+        public void GetTraining()
+        {
+            Console.WriteLine("Dieta de terça e sexta");
+        }
+    }
+}
