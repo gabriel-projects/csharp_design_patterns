4c2cf2a057740e385428c04d529f8dda8c402d36
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2023-10-12 23:07:17 -0300

    inicio abstract factory

---
+++ Udemy_1/Creational/DesignPatterns/PatternsCriational/AbstractFactory/Criational/Factories/TuesdayFridayFactory.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.PatternsCriational.AbstractFactory.Criational.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.PatternsCriational.AbstractFactory.Criational.Factories
+{
+    public class TuesdayFridayFactory : IFactory
+    {
+        public void DailyRoutines()
+        {
+
+        }
+    }
+}
---
+++ Udemy_1/Creational/DesignPatterns/PatternsCriational/AbstractFactory/Criational/Interfaces/IFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.PatternsCriational.AbstractFactory.Criational.Interfaces
+{
+    public interface IFactory
+    {
+        void DailyRoutines();
+    }
+}
